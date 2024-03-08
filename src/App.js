ed2291f6a127a255633f203becfa697f792f177a
habibulloh004 <habibulloh4080@gmail.com>
2024-03-08 15:55:21 +0500

    hello world2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import io from "socket.io-client";
 
-const socket = io.connect("http://localhost:3001");
+const socket = io.connect("https://web-socket-server-8k3j.onrender.com");
 function App() {
   const [message, setMessage] = useState("");
   const [recieveMsg, setRecieveMsg] = useState("");
