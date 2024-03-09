e0a3cc998772524724a53b6c200f4fde65c33a21
habibulloh004 <habibulloh4080@gmail.com>
2024-03-09 10:11:42 +0500

    jfldaksjflsa

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
 import { useEffect, useState } from "react";
 import io from "socket.io-client";
 
-const socket = io.connect("https://web-socket-server-8k3j.onrender.com");
+const socket = io.connect("http://localhost:3001");
 function App() {
   const [message, setMessage] = useState("");
   const [recieveMsg, setRecieveMsg] = useState("");
   const [room, setRoom] = useState("");
 
-  // const joinRoom = () => {
-  //   if (room !== "") {
-  //     socket.emit("join_room", room);
-  //   }
-  // };
+  const joinRoom = () => {
+    if (room !== "") {
+      socket.emit("join_room", room);
+    }
+  };
 
   const sendMessage = () => {
     socket.emit("send_message", {
       message,
-      // room,
+      room,
     });
   };
 
   useEffect(() => {
     socket.on("recieve_message", (data) => {
       setRecieveMsg(data.message);
-      console.log(data);
     });
 
     socket.on("userData", (data) => {
@@ -33,9 +32,9 @@ function App() {
 
   return (
     <div className="App">
-      {/* <input placeholder="Room..." onChange={(e) => setRoom(e.target.value)} />
+      <input placeholder="Room..." onChange={(e) => setRoom(e.target.value)} />
       <button onClick={joinRoom}>Join room</button>
-      <br /> */}
+      <br />
       <input
         placeholder="Type..."
         onChange={(e) => setMessage(e.target.value)}
